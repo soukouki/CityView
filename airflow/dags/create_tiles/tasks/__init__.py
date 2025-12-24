9f9027bd0da1b981280c50bf878a0e44143239a1
soukouki <soukouki0@yahoo.co.jp>
2025-12-24 22:27:07 +0900

    Airflowはもう必要無いので削除

--- airflow/dags/create_tiles/tasks/__init__.py
+++
@@ -1,7 +0,0 @@
-from .capture_g import capture_g
-from .estimate_g import estimate_g
-from .tile_cut_g import tile_cut_g
-from .tile_merge_g import tile_merge_g
-from .tile_compress_g import tile_compress_g
-
-__all__ = ['capture_g', 'estimate_g', 'tile_cut_g', 'tile_merge_g', 'tile_compress_g']
